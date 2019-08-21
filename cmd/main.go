65410f21dd7739131ad1787adba69be54b1b141e
Steve Cook <steve.cook@form3.tech>
2019-08-21 17:53:50 +0100

    allow different test types

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/andykuszyk/zipkin-load-test/pkg/loadtest"
-	"net/http"
 )
 
 func main() {
 	sampleSize := flag.Int("n", 1000, "The sample size to use.")
+	test := flag.String("test", "sqs", "Which test implementation to run (zipkin or sqs)")
+	sqsRegion := flag.String("sqsRegion", "eu-west-1", "Which region to use (sqs only)")
+	sqsEndpoint := flag.String("sqsEndpoint", "http://localhost:4100", "Which endpoint to use (sqs only)")
 	flag.Parse()
-	testRunner := loadtest.TestRunner{
-		Client:     &http.Client{},
-		SampleSize: *sampleSize,
+
+	fmt.Printf("Running load test %s\n", *test)
+	var loadTest loadtest.LoadTest
+
+	if *test == "zipkin" {
+		loadTest = &loadtest.ZipkinTest{}
+	} else if *test == "sqs" {
+		loadTest = &loadtest.SqsTest{
+			SQSEndpoint: sqsEndpoint,
+			SQSRegion: sqsRegion,
+		}
 	}
-	testRunner.Run()
+
+	testRunner := loadtest.TestRunner{
+		SampleSize: *sampleSize,
+		Test:       loadTest,
+	}
+	testRunner.Execute()
 }
--- pkg/loadtest/loadtest.go
+++ pkg/loadtest/loadtest.go
@@ -1,81 +1,51 @@
 package loadtest
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/openzipkin/zipkin-go/idgenerator"
-	"github.com/openzipkin/zipkin-go/model"
-	"github.com/openzipkin/zipkin-go/reporter"
 	"github.com/rcrowley/go-metrics"
-	"log"
-	"net/http"
 	"time"
 )
 
-type HttpClient interface {
-	Do(*http.Request) (*http.Response, error)
+type LoadTest interface {
+	Setup()
+	Run()
+	Teardown()
 }
 
 type TestRunner struct {
-	Client     HttpClient
 	SampleSize int
+	Test       LoadTest
 }
 
-func NewSpan() model.SpanModel {
-	idGen := idgenerator.NewRandom64()
-	traceId := idGen.TraceID()
-	return model.SpanModel{
-		SpanContext:    model.SpanContext{
-			TraceID:  traceId,
-			ID:       idGen.SpanID(traceId),
-		},
-		Name:           "foo",
-		Kind:           model.Client,
-		Timestamp:      time.Now(),
-	}
-}
-
-func (r *TestRunner) Run() {
+func (r *TestRunner) Execute() {
 	s := metrics.NewUniformSample(r.SampleSize)
 	h := metrics.NewHistogram(s)
 	metrics.Register("histo", h)
 
-	for i := 0; i < r.SampleSize; i++ {
-		fmt.Printf("\rProcessing span %d/%d", i + 1, r.SampleSize)
-		span := NewSpan()
-		batch := []*model.SpanModel{&span}
-		body, err := reporter.JSONSerializer{}.Serialize(batch)
-		if err != nil {
-			log.Fatal(err)
-		}
-		req, err := http.NewRequest("POST", "http://localhost:9411/api/v2/spans", bytes.NewReader(body))
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Header.Set("Content-Type", "application/json")
+	r.Test.Setup()
 
+	for i := 0; i < r.SampleSize; i++ {
+		fmt.Printf("\rProcessing iteration %d/%d", i + 1, r.SampleSize)
 		start := time.Now()
-		resp, err := r.Client.Do(req)
+
+		r.Test.Run()
+
 		h.Update(time.Since(start).Nanoseconds())
 
-
-		if err != nil {
-			log.Fatalf("\r\nError when posting span: %s", err)
-		}
-		if resp.StatusCode != 202 {
-			log.Fatalf("\r\nUnexpected status code when posting span: %d", resp.StatusCode)
-		}
 	}
+
+	r.Test.Teardown()
+
 	fmt.Println("")
 	ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
 	fmt.Printf("  count:       %9d\n", h.Count())
-	fmt.Printf("  min:         %9d\n", h.Min() / 1000000)
-	fmt.Printf("  max:         %9d\n", h.Max() / 1000000)
-	fmt.Printf("  mean:        %12.2f\n", h.Mean() / 1000000)
-	fmt.Printf("  stddev:      %12.2f\n", h.StdDev() / 1000000)
-	fmt.Printf("  median:      %12.2f\n", ps[0] / 1000000)
-	fmt.Printf("  75%%:         %12.2f\n", ps[1] / 1000000)
-	fmt.Printf("  95%%:         %12.2f\n", ps[2] / 1000000)
-	fmt.Printf("  99%%:         %12.2f\n", ps[3] / 1000000)
-	fmt.Printf("  99.9%%:       %12.2f\n", ps[4] / 1000000)
+	fmt.Printf("  min:         %12.2fms\n", float64(h.Min()) / 1000000)
+	fmt.Printf("  max:         %12.2fms\n", float64(h.Max()) / 1000000)
+	fmt.Printf("  mean:        %12.2fms\n", h.Mean() / 1000000)
+	fmt.Printf("  stddev:      %12.2fms\n", h.StdDev() / 1000000)
+	fmt.Printf("  median:      %12.2fms\n", ps[0] / 1000000)
+	fmt.Printf("  75%%:         %12.2fms\n", ps[1] / 1000000)
+	fmt.Printf("  95%%:         %12.2fms\n", ps[2] / 1000000)
+	fmt.Printf("  99%%:         %12.2fms\n", ps[3] / 1000000)
+	fmt.Printf("  99.9%%:       %12.2fms\n", ps[4] / 1000000)
 }
